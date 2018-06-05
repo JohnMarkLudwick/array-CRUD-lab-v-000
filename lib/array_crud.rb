3cce948d7356f8cab7e3baaf9cefb7ad9c871029
JohnMark <johnmarkludwick@gmail.com>
2018-06-05 04:55:15 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  chickens = ["billy", "susan", "jimmy", "fritz"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  []
+  array << "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
